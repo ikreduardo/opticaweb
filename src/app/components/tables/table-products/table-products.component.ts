cbc46f3697f46023165bbf424b45bbc1c3af416a
Jose Luis Martinez R <jose_martinezri@hotmail.com>
2020-08-10 00:43:17 -0500

    ok test

--- src/app/components/tables/table-products/table-products.component.ts
+++ src/app/components/tables/table-products/table-products.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { Product } from "../../../models/product";
+//import { Product } from "../../../models/product";
+import { Router } from "@angular/router";
+import { FormGroup, Validators, FormControl } from "@angular/forms";
+import { ProductsService } from "src/app/services/products.service"
 
 @Component({
   selector: 'app-table-products',
@@ -7,46 +10,24 @@ import { Product } from "../../../models/product";
   styleUrls: ['./table-products.component.css']
 })
 export class TableProductsComponent implements OnInit {
-  productsArray : Product[] = [
-    {id: 1, name: "lente graduacion +1", description: "lentes elegantes", price: 200, discount: 49 },
-    {id: 2, name: "lente graduacion +1.5", description: "lentes con antireflejante", price: 300, discount: 49 },
-    {id: 3, name: "lente graduacion +3", description: "lentes tipo aviador", price: 450, discount: 49 }
-  ];
+  FormProduct: FormGroup;
 
-  selectedProduct: Product = new Product();
+  constructor(private router:Router, private pdsrv:ProductsService ){this.createForm();}
 
-  addOrEdit(){
-    if(this.selectedProduct.id == 0){
-      this.selectedProduct.id = this.productsArray.length + 1;
-      this.productsArray.push(this.selectedProduct);
-    }
-    else{
-      if(confirm('Deseas actualizar los datos de '+this.selectedProduct.name.toString())){
-        this.selectedProduct = new Product();
-      }
-    }
-    
-    
-  }
-
-  openForEdit(product: Product){
-    this.selectedProduct = product
-  }
-
-  delete(){
-    if(confirm('Deseas eliminar '+this.selectedProduct.name.toString())){
-      this.productsArray = this.productsArray.filter(x => x != this.selectedProduct);
-      this.selectedProduct = new Product();
-    }
-  }
-
-  title= 'Administrador';
-  constructor() { 
-    
+  createForm() {
+    this.FormProduct = new FormGroup({
+      name: new FormControl('', Validators.compose([Validators.required])),
+      brand: new FormControl('', Validators.compose([Validators.required])),
+      model: new FormControl('', Validators.compose([Validators.required])),
+      description: new FormControl('', Validators.compose([Validators.required])),
+      price: new FormControl('', Validators.compose([Validators.required])),
+      discount: new FormControl('', Validators.compose([Validators.required])),
+      optic_id: new FormControl('', Validators.compose([Validators.required])),
+    })
   }
 
   cleanForm(){
-    this.selectedProduct = new Product();
+    //this.selectedProduct = new Product();
   }
 
   ngOnInit(): void {
--- src/app/models/product.ts
+++ src/app/models/product.ts
@@ -1,7 +1,7 @@
-export class Product {
+export interface IProduct {
     id: number;
     name: string;
     description: string;
     price: number;
     discount:number;
-}
+  }
