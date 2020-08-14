39c9dd9b16015c88c7bfef336bcd01560fc21eaa
ikreduardo <ikreduardo93@gmail.com>
2020-08-14 00:23:05 -0500

    actualizacion

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { Product } from '../models/product';
+import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsService {
 
-  constructor() { }
+  private clientes: Product[];
+  endpoint = environment.endPoint;
+  constructor(private http:HttpClient) { }
+
+  getallProducts() {
+    const path = this.endpoint + '/api/view_products'
+    return this.http.get<any>(path);
+  }
 }
